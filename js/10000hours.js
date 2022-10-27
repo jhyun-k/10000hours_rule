e382f37222d6bbec65d9fa7e1d89f76ed9cba0a6
jhyun-k <jhkim1238@naver.com>
2022-10-27 10:16:23 +0900

    로딩창 추가

--- js/10000hours.js
+++ js/10000hours.js
@@ -2,24 +2,39 @@ const inp_text = document.querySelector('.text');
 const inp_time = document.querySelector('.time');
 const bigTxt1 = document.querySelector('.big_txt1')
 const bigTxt2 = document.querySelector('.big_txt2')
+
 const yellowBtn = document.querySelector('.yellow_btn')
 const goBtn = document.querySelector('.go_btn')
 const endBtn = document.querySelector('.end_btn')
+
 const modal = document.querySelector('.cont-modal')
 const result = document.querySelector('.result')
 
-yellowBtn.addEventListener('click',()=>{
-    let time = inp_time.value   
-    console.log(time)
-    bigTxt1.textContent = inp_text.value;
-    bigTxt2.textContent = Math.round(10000/time)
+const loadImg = document.querySelector('.loading_img')
 
-    if(inp_text.value==''|| time==''){
-        alert('값을 입력해주세요')
-    }else{
-    result.style.display='block'
-    }
+yellowBtn.addEventListener('click',()=>{
+    result.style.display='none'
+    loadImg.classList.add('show');
+
+    setTimeout(() => {
+        loadImg.classList.remove('show')
+    }, 1500);
+
+    setTimeout(() => {
+        let time = inp_time.value   
+        console.log(time)
+        bigTxt1.textContent = inp_text.value;
+        bigTxt2.textContent = Math.round(10000/time)
+    
+        if(inp_text.value==''|| time==''){
+            alert('값을 입력해주세요')
+        }else{
+        result.style.display='block'
+        }
+    }, 1500);
+
 })
+
 
 goBtn.addEventListener('click',()=>{
     modal.style.display = 'block'
